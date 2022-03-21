43dfe0075b1ff16e4051f1a178a3a557da75a2c1
panyulong <527875893@qq.com>
2022-03-21 18:02:16 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,83 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-	transpileDependencies: true,
-	pages: {
-		main: {
-			// 入口js
-			entry: "src/modules/main/main.ts",
-			// 模板来源
-			template: "public/main.html",
-			// 在 dist 中生成的html文件名字
-			filename: "main.html",
-			// template html 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-			title: "Main Page",
+const path = require("path");
+
+export default defineConfig(({ mode }) => {
+	const { VITE_APP_BASE_URL } = loadEnv(mode, process.cwd());
+	return {
+		base: VITE_APP_BASE_URL,
+		plugins: [vue()], //VueSetupExtend()
+		transpileDependencies: true,
+		lintOnSave: false,
+		build: {
+			chunkSizeWarningLimit: 1000,
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return id
+								.toString()
+								.split("node_modules/")[1]
+								.split("/")[0]
+								.toString();
+						}
+					},
+				},
+			},
 		},
-		remind: {
-			entry: "src/modules/remind/remind.ts",
-			template: "public/remind.html",
-			filename: "remind.html",
-			title: "Remind Page",
+		pages: {
+			main: {
+				// 入口js
+				entry: "src/modules/main/main.ts",
+				// 模板来源
+				template: "public/main.html",
+				// 在 dist 中生成的html文件名字
+				filename: "main.html",
+				// template html 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+				title: "Main Page",
+			},
+			remind: {
+				entry: "src/modules/remind/remind.ts",
+				template: "public/remind.html",
+				filename: "remind.html",
+				title: "Remind Page",
+			},
 		},
-	},
+		server: {
+			open: true,
+			host: true,
+		},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "src"),
+				"@c": path.resolve(__dirname, "src/components"),
+			},
+		},
+		css: {
+			preprocessorOptions: {
+				less: {
+					charset: false,
+					javascriptEnabled: true,
+					additionalData: `@import '${path.resolve(
+						__dirname,
+						"src/assets/css/common.less"
+					)}';`,
+				},
+			},
+			charset: false,
+			postcss: {
+				plugins: [
+					{
+						postcssPlugin: "internal:charset-removal",
+						AtRule: {
+							charset: (atRule) => {
+								if (atRule.name === "charset") {
+									atRule.remove();
+								}
+							},
+						},
+					},
+				],
+			},
+		},
+	};
 });
